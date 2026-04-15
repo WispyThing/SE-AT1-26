30f9b79deece79a6bde8256b3ef9411f088ba800
WispyThing <oscar.perez7@education.nsw.gov.au>
2026-04-16 02:22:38 +1000

    Game is finished! Went late into the night doing this because I was locked in, but it's finished now! I did have to research a lot of stuff and find some weird code to do some of this, but it all works now!

--- Classes.py
+++ Classes.py
@@ -2,10 +2,8 @@ import random
 
 score = 0
 
-minigame_chosen = 0
-
 class Questions:
-    def __init__(self, number, words, answer, correct_answer, points):
+    def __init__(self, number, words, answer, correct_answer, feedback, points):
 
         self.number = number
 
@@ -14,6 +12,8 @@ class Questions:
         self.answer = answer
 
         self.__correct_answer = correct_answer
+
+        self.feedback = feedback
 
         self.__points = points
 
@@ -41,14 +41,22 @@ class Questions:
 
     def askQuestion(self):
         while True:
-            self.answer = input(f"Question {self.number}. {self.words} ")
+            self.answer = input(f"Question {self.number}. {self.words} ").lower()
+            
                 
             if self.answer in ["yes", "no"]:
                 if self.answer == self.__correct_answer:
+                    print("")
                     print("Correct!")
+                    print("")
                     self.addPoints()
+                    print("")
                 else:
+                    print("")
                     print("Incorrect.")
+                    print("")
+                    print(f"Here is some feedback! {self.feedback}")
+                    print("")
                 return self.answer
             else:
                 print("Invalid input. Please type 'yes' or 'no'.")
@@ -56,23 +64,29 @@ class Questions:
     
 class Minigames(Questions):
     
-    def __init__(self, mini_number, words, answer, correct_answer,):
+    def __init__(self, number, words, answer, correct_answer, feedback):
 
-        super().__init__(mini_number, words, answer, correct_answer, 10)
+        super().__init__(number, words, answer, correct_answer, feedback, 10)
 
-        self.mini_number = mini_number
 
 
     def askQuestion(self):
         while True:
-            self.answer = input(f"You've encountered minigame {self.mini_number}! {self.words} ")
+            self.answer = input(f"You've encountered minigame {self.number}! {self.words} ").lower()
                 
             if self.answer in ["yes", "no"]:
-                if self.answer == self.__correct_answer:
+                if self.answer == self.getCorrectAnswer():
+                    print("")
                     print("Correct!")
+                    print("")
                     self.addPoints()
+                    print("")
                 else:
+                    print("")
                     print("Incorrect.")
+                    print("")
+                    print(f"Here is some feedback! {self.feedback}")
+                    print("")
                 return self.answer
             else:
                 print("Invalid input. Please type 'yes' or 'no'.")
@@ -85,30 +99,22 @@ class Minigames(Questions):
         
         print(f"Your new score is {score}.")
 
-    def minigameSelection(self):
 
-        minigame_chosen = random.randint(1, 5)
+def minigameSelection():
 
-        if minigame_chosen == 1:
-            Minigame1.askQuestion()
-        elif minigame_chosen == 2:
-            Minigame2.askQuestion()
-        elif minigame_chosen == 3:
-            Minigame3.askQuestion()
-        elif minigame_chosen == 4:
-            Minigame4.askQuestion()
-        elif minigame_chosen == 5:
-            Minigame5.askQuestion()
+    global available_minigames
+    chosen = random.choice(available_minigames)
+    available_minigames.remove(chosen)
+    chosen.askQuestion()
 
-    
+Question1 = Questions(1, "You have a test coming up, do you study the days before the test?", "", "yes", "It's always important to study before a test to ensure you know your content!", 25)
+Question2 = Questions(2, "Your test is tomorrow! Do you sleep in the night before the test?", "", "yes", "Good rest before a test is important as it improves your cognitive function on the day of the test!", 25)
+Question3 = Questions(3, "It's the day of the test! Your friend tells you to cheat on the test, do you cheat?", "", "no", "Never cheat on a test! If you get caught it is often an instant zero! Hard work will always beat cheating!", 50)
 
-Question1 = Questions(1, "You have a test coming up, do you study the days before the test?", "", "yes", 25)
-Question2 = Questions(2, "Your test is tomorrow! Do you sleep in the night before the test?", "", "yes", 25)
-Question3 = Questions(3, "It's the day of the test! Your friend tells you to cheat on the test, do you cheat?", "", "no", 50)
+Minigame1 = Minigames(1, "You see an old lady waiting to cross a road, do you help her?", "", "yes", "Make sure to always help people around you when they look like they need help!")
+Minigame2 = Minigames(2, "You have homework due soon! Do you ditch it to hang out with your friends?", "", "no", "While hanging out with your friends and making memories is always important, you need to make sure you are always caught up on schoolwork! It is ok to take breaks every now and again though!.")
+Minigame3 = Minigames(3, "You're impatient waiting in line in the canteen, do you skip to the front of the line?", "", "no", "If everyone else is waiting patiently, don't cut in, respect their time as much as yours!")
+Minigame4 = Minigames(4, "Your mate wants you to skip class, do you tag along?", "", "no", "Skipping even one class can set you very behind in your learning! It is important that you attend all of your classes when you are able to!")
+Minigame5 = Minigames(5, "You have a question for your teacher but you aren't sure on whether you should ask it or not, do you ask it?", "", "yes", "It is always important to ask quesitons when you are unsure of something, there are no stupid questions!")
 
-Minigame1 = Minigames(1, "", "", "")
-Minigame2 = Minigames(2, "", "", "")
-Minigame3 = Minigames(3, "", "", "")
-Minigame4 = Minigames(4, "", "", "")
-Minigame5 = Minigames(5, "", "", "")
-
+available_minigames = [Minigame1, Minigame2, Minigame3, Minigame4, Minigame5]
--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import random
 import Classes
 
+print("Welcome to Oscar's super duper ultra mega cool educational game about tests (and some other stuff)!\nYou must travel around the map using w, a, s, and d, to reach the three main locations (1, 2 and 3) to win!\nYou can also go to minigame locations (M) to score some extra points!")
+print("")
+
 game_map = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
@@ -16,14 +19,27 @@ game_map[random.randint(0, 4)][random.randint(0, 4)] = 'P'
 #List of locations to place
 locations = [1, 2, 3]
 
+#List of locations which have been visited by the player
+visited_locations = []
+
 #Places three locations locations on the map
 for location_id in locations:
     placed = False
     while placed == False:
         row, column = random.randint(0, 4), random.randint(0, 4)
         if game_map[row][column] == 0:
-            game_map[row][column] = location_id
-            placed = True
+                game_map[row][column] = location_id
+                placed = True
+
+#Pretty much just copy pasted the code above and tweaked it so we have three new minigame locations
+for M in locations:
+    placed = False
+    while placed == False:
+        row, column = random.randint(0, 4), random.randint(0, 4)
+        if game_map[row][column] == 0:
+                game_map[row][column] = 'M'
+                placed = True
+
 
 for row in game_map:
     #Had to look up how to do this wizadry but it gets rid of all the ugly parts of the map
@@ -38,7 +54,9 @@ for row in range(5):
 
 #Movement Loop
 while True:
+    #Takes users input to move, little tail on the end allows them to use capitals letters and it will still work!
     move = input("\nMove (w/a/s/d): ").lower()
+    print("")
 
     #Store old position to clear it
     old_row = player_row
@@ -56,18 +74,36 @@ while True:
     
     #Checks if the player has moved to a location before overwriting
     destination = game_map[player_row][player_column]
+    
+    #Asks the questions
     if destination in [1, 2, 3]:
-        print(f"*** You found location {destination}! ***")
+        #Puts to location in in the "visited_locations" array so that we can trigger the win condition
+        if destination not in visited_locations:
+            visited_locations.append(destination)
 
-        if destination == 1:
-            Classes.Question1.askQuestion()
-        
-        elif destination == 2:
-            Classes.Question2.askQuestion()
+            print(f"You found location {destination}!")
+            print("")
 
-        elif destination == 3:
-            Classes.Question3.askQuestion()
+            if destination == 1:
+                Classes.Question1.askQuestion()
             
+            elif destination == 2:
+                Classes.Question2.askQuestion()
+
+            elif destination == 3:
+                Classes.Question3.askQuestion()
+
+    #Begins a minigame
+    elif destination == 'M':
+        print("You found a minigame!")
+        print("")
+        Classes.minigameSelection()       
+    
+    #The win condition, if the length of the "visited_locations" array is 3 (meaning all three locations have been visited) the game ends
+    if len(visited_locations) == 3:
+        print(f"You completed all locations! You win! Your score is {Classes.score}!")
+        break
+
 
 
     #Updates the map
